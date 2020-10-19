147f00f3396d2d0e958f6f7e0859e48d3930f3a0
Lorenzo García Moreno <dev@lorenzogm.com>
2020-10-20 00:44:39 +0200

    fix(config): mainNavigation

--- src/contexts/settings-context.tsx
+++ src/contexts/settings-context.tsx
@@ -1,23 +1,35 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react'
+
+const mainNavigation = [
+  {
+    type: 'folder',
+    name: 'Mujer y Hombre',
+    path: '/mujer-y-hombre',
+  },
+  {
+    type: 'folder',
+    name: 'Peques',
+    path: '/peques',
+  },
+  {
+    type: 'folder',
+    name: 'Bebés',
+    path: '/bebes',
+  },
+]
 
 // A simple context for handling the current settings
 export const SettingsContext = React.createContext({
-  currency: 'eur',
-  mainNavigation: []
-});
-
-export const useSettings = () => useContext(SettingsContext);
-
-export const SettingsProvider = ({
-  currency: cur,
+  currency: 'EUR',
   mainNavigation,
-  children
-}) => {
-  const [currency, setCurrency] = useState(cur);
+})
+
+export const useSettings = () => useContext(SettingsContext)
+
+export const SettingsProvider = ({ currency: cur, children }) => {
+  const [currency, setCurrency] = useState(cur)
 
   return (
-    <SettingsContext.Provider value={{ currency, setCurrency, mainNavigation }}>
-      {children}
-    </SettingsContext.Provider>
-  );
-};
+    <SettingsContext.Provider value={{ currency, setCurrency, mainNavigation }}>{children}</SettingsContext.Provider>
+  )
+}
