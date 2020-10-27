73625d5394e1c64eed833e33d00d5fa80fe24834
Lorenzo García Moreno <dev@lorenzogm.com>
2020-10-27 15:25:08 +0100

    fix(config): rename "mainNavigation" to "navigation"

--- src/themes/dindim/config/navigation.ts
+++ src/themes/dindim/config/navigation.ts
@@ -1,4 +1,4 @@
-const mainNavigation = [
+const navigation = [
   {
     type: 'folder',
     name: 'Grandes',
@@ -21,4 +21,4 @@ const mainNavigation = [
   },
 ]
 
-export default mainNavigation
+export default navigation
