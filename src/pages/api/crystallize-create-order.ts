970ce5d6b7bd751b554cdb973ede71259f3c7c6f
Lorenzo García Moreno <dev@lorenzogm.com>
2020-10-31 00:41:34 +0100

    enable order creation

--- src/pages/api/crystallize-create-order.ts
+++ src/pages/api/crystallize-create-order.ts
@@ -1,17 +1,21 @@
 import { createCrystallizeOrder } from 'lib-api/crystallize/order'
 import { emailOrderConfirmation } from 'lib-api/emails'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { order, deliveryMethod } = req.body
-    // const createCrystallizeOrderResponse = await createCrystallizeOrder(order)
+    const createCrystallizeOrderResponse = await createCrystallizeOrder(order)
 
-    await emailOrderConfirmation({ orderId: '5f9c143bb7a3b4001c8a68ad', order, deliveryMethod })
-    // await emailOrderConfirmation({ orderId: createCrystallizeOrderResponse.data.orders.create.id, order })
+    await emailOrderConfirmation({
+      orderId: createCrystallizeOrderResponse.data.orders.create.id,
+      order,
+      deliveryMethod,
+    })
 
     return res.status(200).send({
       success: true,
-      // ...createCrystallizeOrderResponse,
+      ...createCrystallizeOrderResponse,
     })
   } catch (error) {
     return res.status(503).send({
